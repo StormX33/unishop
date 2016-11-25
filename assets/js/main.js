5d77f8deacbe6c00e45a4ce94af975ec73d9e3c0
zoryana-petrova <zoryana.petrova@gmail.com>
2016-11-25 21:44:26 +0300

    Updated banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
-// $(document).ready(function() {
-//     if ($(".filter").length) {
-//         $(".filter__item").addClass("active");
-        
-//         $(".filter__title").on("click", function(e) {
-//             var $this = $(this),
-//                 container = $this.closest(".filter__item"),
-//                 content = container.find(".filter__content");
-
-//             container.toggleClass('active');
-//             content.stop(true, true).slideToggle();
-//         });
-//     }
-// });
-
-
-
-
 // Header dropdown
 $(function () {
     var $phoneHandler = $('.contacts__tel'),
